e67b4d00e9acc6850bd7c5bf69b2c86f84314ccd
rapaMatiase <rapa.matias.e@gmail.com>
2021-10-31 15:24:03 -0300

    Instale native base y is workinggit add .

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { NativeBaseProvider, Box } from 'native-base';
 
 
 function WelcomeScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
+     
+      <NativeBaseProvider>
+        <Box>Hello world</Box>
+       </NativeBaseProvider>
     );
   }
 
