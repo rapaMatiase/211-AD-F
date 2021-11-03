ce4f393adcdf47ec7f72275516579d4e60bfe6f2
rapaMatiase <rapa.matias.e@gmail.com>
2021-11-03 01:09:32 -0300

    No funciona la camara

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,30 +6,28 @@ import {
 } from "native-base";
 import {  PermissionsAndroid, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
-const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "Cool Photo App Camera Permission",
-          message:
-            "Cool Photo App needs access to your camera " +
-            "so you can take awesome pictures.",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the camera");
-      } else {
-        console.log("Camera permission denied");
+import Camera from 'react-native-camera';
+
+async function requestCameraPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Cool Photo App Camera Permission',
+        message:
+          'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.',
       }
-    } catch (err) {
-      console.warn(err);
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the camera');
+    } else {
+      console.log('Camera permission denied');
     }
-  };
-  
+  } catch (err) {
+    console.warn(err);
+  }
+}  
 const HomeScreen = () => {
 
     return (
